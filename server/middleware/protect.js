97376f90d925e5795e3c379ce7c41046c511c01e
Sylvia Lee <sylvieelee@gmail.com>
2026-02-19 11:32:34 -0500

    trying to protect endpoints

---
+++ server/middleware/protect.js
@@ -0,0 +1,25 @@
+import { expressjwt } from 'express-jwt';
+
+import jwksRsa from 'jwks-rsa';
+
+
+const checkJwt = expressjwt({
+
+  secret: jwksRsa.expressJwtSecret({
+
+    cache: true,
+
+    rateLimit: true,
+
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+
+  }),
+
+  audience: process.env.AUTH0_AUDIENCE,
+
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+
+  algorithms: ['RS256']
+
+
+});
