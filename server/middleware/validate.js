38d3aabb924b74e0d04831112dd2b92d65ed0001
Sophia Kim <sophia.el.kim@gmail.com>
2026-05-30 12:07:06 -0400

    added saturday ip

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -13,8 +13,9 @@ const ALLOWED_IPS = [
 const ALLOWED_CIDR_RANGES = [
   '168.229.254.0/24',  // BCA BYOD network (covers .0 - .255, includes .66 and .67)
   // '192.168.1.0/24', // add korean school ip address when i actually find out
-  '24.38.110.0/24',    // Korean school network
-  '146.71.0.0/24' // kajeet smartbus
+  '24.38.110.0/24',    // Korean school network friday
+  '146.71.0.0/24', // kajeet smartbus
+  '47.19.134.250', // korean school network saturday (tsguest)
 ];
 
 // check if the user's IP falls within a CIDR range
